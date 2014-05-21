5016a848140369ed7056c3ae9de127fa237bf6d4
Evan Lloyd <eulloyd@gmail.com>
2014-05-22 08:07:04 +0900

    update redirect syntax

--- posts.rb
+++ posts.rb
@@ -2,6 +2,6 @@ require 'csv'
 
 CSV.foreach('postdata.csv') do |row|
 	CSV.open('postoutput.csv', 'a+') do |rownew|
-    rownew << ["if ( $request_filename ~ #{row[1]} ) { rewrite ^ #{row[0]} permanent; }"] 
+    rownew << ["rewrite #{row[1]} #{row[0]} permanent;"] 
 	end
 end
